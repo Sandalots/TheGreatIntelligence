6ea867001f6b00078ace1968396a4167155235a7
Sandy MacDonald <59518103+Sandalots@users.noreply.github.com>
2022-06-30 23:18:03 +0100

    ADDED CLEAR

--- TheGreatIntelligence/main.js
+++ TheGreatIntelligence/main.js
@@ -149,3 +149,7 @@ function generateItem() {
   var randomItem = episodeList[Math.floor(Math.random()*episodeList.length)];
   document.getElementById("episode").innerHTML += "<br>" + randomItem;
 }
+
+function clearItem() {
+  document.getElementById("episode").innerHTML = "";
+}
