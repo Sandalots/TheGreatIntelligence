36a494fa24d32fa9f71fa5ff0a0550738cf23153
Sandy MacDonald <59518103+Sandalots@users.noreply.github.com>
2022-07-01 22:55:38 +0100

    Update classics.js

--- docs/scripts/classics.js
+++ docs/scripts/classics.js
@@ -1,5 +1,21 @@
 var episodeList = [
     "An Unearthly Child",
+    "The Daleks",
+    "The Edge of Destruction",
+    "Marco Polo",
+    "The Keys of Marinus",
+    "The Aztecs",
+    "The Sensorites",
+    "The Reign of Terror",
+    "Planet of Giants",
+    "The Dalek INvasion of Earth",
+    "The Rescue",
+    "The Romans",
+    "The Web Planet",
+    "The Crusade",
+    "The Space Museum",
+    "The Chase",
+    "THe Time Meddler"
 
   ];
   
