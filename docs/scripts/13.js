dd68dc0a99a4fb9c0dcc58c1d5d8f468a9017f86
Sandy MacDonald <59518103+Sandalots@users.noreply.github.com>
2022-09-06 00:04:57 +0100

    Added 13's content.

---
+++ docs/scripts/13.js
@@ -0,0 +1,45 @@
+var episodeList = [
+    "The Woman Who Fell to Earth",
+    "The Ghost Monument",
+    "Rosa",
+    "Arachnids in the UK",
+    "The Tsuranga Conundrum",
+    "Demons of the Punjab",
+    "Kerblam!",
+    "The Witchfinders",
+    "It Takes You Away",
+    "The Battle of Ranskoor Av Kolos",
+    "Resolution",
+    "Spyfall Part 1",
+    "Spyfall Part 2",
+    "Orphan 55",
+    "Nikola Tesla's Night of Terror",
+    "Fugitive of the Judoon",
+    "Praxeus",
+    "Can You Hear Me?",
+    "The Haunting of Villa Diodati",
+    "Ascension of the Cybermen",
+    "The Timeless Children",
+    "Revolution of the Daleks",
+    "The Halloween Apocalypse",
+    "War of the Sontarans",
+    "Once, Upon a Time",
+    "Village of the Angels",
+    "Survivors of the Flux",
+    "The Vanquishers",
+    "Eve of the Daleks",
+    "Legend of the Sea Devils"
+
+  ];
+  
+  function generateItem() {
+    var randomItem = episodeList[Math.floor(Math.random()*episodeList.length)];
+    document.getElementById("episode").innerHTML += "<br>" + randomItem;
+  }
+  
+  function clearItem() {
+    document.getElementById("episode").innerHTML = "";
+    
+  }
+  
+  
