04a8e4c938430de6cf0b2735fb80b21c1e8fe3a4
Sandy MacDonald <59518103+Sandalots@users.noreply.github.com>
2022-11-14 23:47:07 +0000

    Implemented k9

---
+++ docs/scripts/k9.js
@@ -0,0 +1,15 @@
+var episodeList = [
+    "The Woman Who Fell to Earth"
+  ];
+  
+  function generateItem() {
+    var randomItem = episodeList[Math.floor(Math.random()*episodeList.length)];
+    document.getElementById("episode").innerHTML += "<br>" + randomItem;
+  }
+  
+  function clearItem() {
+    document.getElementById("episode").innerHTML = "";
+    
+  }
+  
+  
