5013db6cb74cc1bb57f44dfb856bfaa9d92195b1
Sandy MacDonald <59518103+Sandalots@users.noreply.github.com>
2022-11-14 23:50:55 +0000

    Updated k9 dataset

--- docs/scripts/k9.js
+++ docs/scripts/k9.js
@@ -1,5 +1,30 @@
 var episodeList = [
-    "The Woman Who Fell to Earth"
+    "Regeneration",
+    "Liberation",
+    "The Korven",
+    "The Bounty Hunter",
+    "Sirens of Ceres",
+    "Fear Itself",
+    "The Fall of The House of Gryffen",
+    "Jaws of Orthrus",
+    "Dream-Eaters",
+    "Curse of Anubis",
+    "Oroborus",
+    "Alien Avatar",
+    "Aeolian",
+    "The Last Oak Tree",
+    "Black Hunger",
+    "The Cambridge Spy",
+    "Lost Library of Ukko",
+    "Mutant Copper",
+    "The Custodians",
+    "Taphony and the Time Loop",
+    "Robot Gladiators",
+    "Mind Snap",
+    "Angel of The North",
+    "The Last Precinct",
+    "Hound of the Korven",
+    "The Eclipse of the Korven"
   ];
   
   function generateItem() {
